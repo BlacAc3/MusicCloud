e202a6a585230e8732f04d8bcecc6d8143e97665
BlacAc3 <aceezeala@gmail.com>
2024-02-09 20:27:02 +0100

    changed media url directive

--- musicplayer/urls.py
+++ musicplayer/urls.py
@@ -24,4 +24,8 @@ urlpatterns = [
     path("", include("player.urls")),
     path("__reload__/", include("django_browser_reload.urls")),
     path("unicorn/", include("django_unicorn.urls")),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
